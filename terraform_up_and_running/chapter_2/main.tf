ad2e176ea2ed9531211583505862ef4c14336c15
mwangigitau <willitimgitau@gmail.com>
2024-01-15 06:38:36 +0300

    More learning

--- terraform_up_and_running/chapter_2/main.tf
+++ terraform_up_and_running/chapter_2/main.tf
@@ -3,14 +3,21 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-05fb0b8c1424f266b"
-  instance_type = "t2.micro"
+  ami                    = "ami-05fb0b8c1424f266b"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data                   = <<-EOF
     #!/bin/bash
     echo "Hello, World" > index.xhtml
-    nohup busybox httpd -f -p 8080 &
+    nohup busybox httpd -f -p ${var.server_port} &
     EOF
   user_data_replace_on_change = true
   tags = {
@@ -22,9 +29,15 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# Output
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
